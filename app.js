31910abf686f40f87cbef5402b1ef72b81bc6394
Hazerin <alby91v@gmail.com>
2023-04-20 21:47:28 +0200

    Tutorial JS in corso

---
+++ app.js
@@ -0,0 +1,68 @@
+/* let isRaining = true
+let temperature = 20
+let planet = "Earth"
+
+temperature += 2
+
+console.log(temperature) */
+
+/* let subscrived = false
+let loggedIn = true
+
+if (subscrived === true) {
+    console.log("show the video")
+}
+else if (loggedIn === true) {
+    console.log("Tell the user to upgrade their subscription")
+}
+else {
+    (console.log("tell user to log into account"))
+} */
+
+/*
+
+let cash = 60
+let price = 40
+
+// Notare l'uso della backslash per non far chiudere la stringa, ma si può evitare con le doppie virgolette di dover usare questa tecnica
+
+if (cash > price ) {
+    console.log(`you paid extra - here\'s your change: ${cash - price} dollars`)
+}
+else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day!")
+}
+else {
+    console.log("not enough money - you still owe ${price - cash} dollars")
+} */
+
+/* let cash = 50
+let price = 40
+let isStoreOpen = true */
+
+// Non è necessario fare isStoreOpen === true, visto che isStoreOpen di per sè ritorna true. Per il false si può usare !isStoreOpen
+
+/* if (cash >= price && isStoreOpen) {
+    console.log("print the receipt")
+} */
+
+/* let hot = true
+
+hot ? console.log('weather is hot outside') : console.log('weather is cold outside') */
+
+/* let cash = 100
+let price = 50
+let isStoreOpen = false
+
+console.log(isStoreOpen && price <= cash ? "give receipt" : "do not give receipt") */
+
+let count = 1
+
+while (count <= 10) {
+    console.log(count)
+    count = count + 1
+}
+
+for (let i = 0; i <= 3; i++) {
+    console.log(i)
+}
