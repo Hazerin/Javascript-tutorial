782f289ee0506b25f80dd0b619329c01ee3e1d6d
Hazerin <alby91v@gmail.com>
2023-04-21 20:09:48 +0200

    Crash course concluso

--- app.js
+++ app.js
@@ -99,3 +99,188 @@ for (let i = 1; i <= 20; i++) {
         console.log(`${i} -> ${i}`)
     }
 } */
+
+/* const str = "Frontend Simplified"
+
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i])
+} */
+
+/* function Welcome (name, lastName) {
+    console.log(`Welcome to Mugenri, ${name} ${lastName}`)
+}
+Welcome("Hazerin", "Haze")
+
+function fn() {
+    return "my return"
+    console.log(`my function`)
+}
+console.log(fn())
+
+function sumOfTwoNumbers(num1, num2) {
+    return num1 + num2
+}
+console.log(sumOfTwoNumbers(10, 15))
+
+function CelsiusToFahrenheit (num) {
+    return num * 1.8 + 32
+}
+console.log(CelsiusToFahrenheit(10))
+
+const CelsiusToFahrenheit2 = (num) => {
+    return num * 1.8 + 32
+}
+console.log(CelsiusToFahrenheit2(0)) */
+
+/* let arr = [20, 30, 40, 50, 100, "hello"]
+
+console.log(arr[0])
+console.log(arr[arr.length-1])
+
+arr.push(200)
+
+console.log(arr) */
+
+/* let arr = [20, 30, 40, 50, 100]
+
+let newArr = arr.filter((element) => {
+    console.log(element)
+    if (element < 50) {
+        return true
+    }
+})
+console.log(newArr) */
+
+/* // metodo veloce. Se l'argomento è uno solo non servono le parentesi intorno ad esso (element in questo caso) e si può evitare di scrivere l'if. 
+let newArr = arr.filter(element => {
+    return element < 50
+})
+ */
+
+// Metodo ancora piu veloce. Se vi e solamente il return nella funzione, e sufficiente scrivere quello che si vuole ritornare dopo la freccia.
+/* let newArr = arr.filter(element => element < 50) */
+
+// Esercizo
+/* const grades = ["A+", "A", "FAIL"]
+
+let goodGrades = grades.filter(element => element !== "FAIL")
+console.log(goodGrades) */
+
+/* let goodGrades2 = []
+
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] !== "FAIL") {
+        goodGrades2.push(grades[i])
+    }
+}
+console.log(goodGrades2) */
+
+/* let arr = [1, 4, 9 , 16]
+
+arr.map((elem) => {
+    console.log(elem)
+})
+
+let new_arr = arr.map(() => "miao") // Se non c'è bisogno di confronto va bene la parentesi vuota
+console.log(new_arr) */
+
+// Devo convertire questi dollari in centesimi, sia col metodo corto che con quello lungo
+/* let dollars = [1, 5, 10, 3]
+let cents = dollars.map((element) => {
+    return element * 100
+})
+console.log(cents)
+
+let cents2 = dollars.map(elem => elem * 100)
+console.log(cents2)
+
+let cents3 = []
+for (let i = 0; i < dollars.length; i++) {
+    cents3.push(dollars[i] * 100)
+}
+console.log(cents3) */
+
+// Oggetti
+
+/* let users = [{
+    username: "Hazerin",
+    email: "Haze@example.com",
+    password: "test123",
+    subscriptionStatus: "VIP",
+    discordId: "Hazered#4678",
+    lessonsCompleted: [0,1],
+} , {
+    username: "Mitri",
+    email: "Mitri@example.com",
+    password: "test123",
+    subscriptionStatus: "VIP",
+    discordId: "Mitri#0001",
+    lessonsCompleted: [0,1,2],
+} , {
+    username: "Zen",
+    email: "Zen@example.com",
+    password: "test123",
+    subscriptionStatus: "Basic",
+    discordId: "Zen#0001",
+    lessonsCompleted: [0,1],
+}]
+
+function login(email, password) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            console.log(users[i])
+            if (users[i].password === password) {
+                console.log("log the user in - the details are correct")
+            }
+            else {
+                console.log("Password is incorrect - try again")
+            }
+            return // evita di loopare tutti gli utenti rimanenti una volta trovato quello corretto
+        }
+    }
+    console.log ("could not find an eamil that matches")
+}
+
+login("Haze@example.com", "test123")
+
+console.log(users[0].username[0])
+console.log(users[0].subscriptionStatus)
+console.log(users[1].lessonsCompleted.map(elem => elem * 2))
+console.log(users[0])
+
+function register (user) {
+    users.push(user)
+}
+
+register ({
+    username: "Miao",
+    email: "Miao@gmail.com",
+    password: "gattomiao",
+    subscriptionStatus: "VIP",
+    discordId: "cat#1111",
+    lessonsCompleted: [0,1,2]
+})
+console.log(users) */
+
+// DOM
+// Primo modo per accedere a un elemento
+/* console.log(document.querySelector("#title"))
+// Secondo modo per accedere un elemento
+console.log(document.getElementById("title")) // Non c'è bisogno di mettere il cancelletto. Funziona solo se l'elemento è un ID.
+
+// Cambiare HTML
+
+document.querySelector("#title").innerHTML = "Gattomiao" // modifica l'interno di questo elemento, io vi avevo scritto Hazerin, ma cosi lo sostituisce con "Gattomiao".
+document.querySelector("#title").innerHTML += " Hazerin" // Aggiunge Hazerin.
+
+// Cambiare CSS
+
+document.querySelector("#title").style.fontSize = "50px"
+
+function changeTitleColor () {
+    document.querySelector("#title").style.color = "green"
+} */
+
+function toggleDarkMode () {
+    document.querySelector("body").classList.toggle("dark-theme")
+}
